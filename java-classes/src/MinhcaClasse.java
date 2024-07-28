df861d503f3a744de4ca7e2339ba56896efa2656
Yuri-Fernandes-dev <devyuri.programador@gmail.com>
2024-07-28 01:01:50 -0300

    commit

---
+++ java-classes/src/MinhcaClasse.java
@@ -0,0 +1,19 @@
+public class MinhcaClasse {                         //classe - nomeclatura intuitiva
+public static void main (String [] args){    //Método
+
+    String primeiroNome = "Yuri";
+    String segundoNome = "Fernandes";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+};
+
+
+
+            // esse metodo foi criado como se fosse uma funcao no javascript para usar em outro lugar
+public static String  nomeCompleto (String primeiroNome, String segundiNonme){   // método
+    return primeiroNome.concat("").concat(segundiNonme);
+}
+
+
+}
